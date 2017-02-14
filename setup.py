3b1b6feac4459969b67c9ce8d9557aeae7f9d552
feilz <akerlunddan@gmail.com>
2017-02-14 10:59:53 +0200

    init commit

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
-[TODO]
+from distutils.core import setup
+
+exec(open("lib/_version.py").read())
+
+setup(name="ds2017",
+    description="ds practice work",
+    version=__version__,
+    author="",
+    author_email="",
+    package_dir={"ds2017chat":"ds2017chat"},
+    packages=["lib"],
+    scripts=["bin/scripts"]
+)
