6b2dd860c1b1d0dfaf04007d9590c9d02b0fe82f
Antti Husa <antti.husa@nixu.com>
2017-02-15 13:21:29 +0200

    Changed program structure and added Debian package build configs

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
-exec(open("lib/_version.py").read())
+exec(open("dschat/_version.py").read())
 
 setup(name="ds2017",
     description="ds practice work",
     version=__version__,
     author="",
     author_email="",
-    package_dir={"ds2017chat":"ds2017chat"},
-    packages=["lib"],
-    scripts=["bin/scripts"]
+    package_dir={"dschat":"dschat"},
+    packages=["dschat"],
+    scripts=["bin/dschat"]
 )
