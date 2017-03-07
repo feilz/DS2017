05b5d376974428192128cf291fb3908479898d79
Antti Husa <antti.husa@nixu.com>
2017-03-07 13:26:35 +0200

    Fixes to import paths. Added requirements and ignore rules for debian build files

--- dschat/daemon/chat_daemon.py
+++ dschat/daemon/chat_daemon.py
@@ -1,10 +1,10 @@
 
-from dschat.main import app
+from dschat.flask import app
 
 
 class ChatDaemon:
     def __init__(self):
-        pass
+        print("Hellow rowld?")
 
     def run(self):
         app.run(host="0.0.0.0", debug=True)
